119ae354c9eb771319d5bd093eed76520ac6bcf5
Nikhil Web <contactnikhil360@gmail.com>
2023-10-22 20:33:26 +0530

    optimized code - use formula

--- workingCode.js
+++ workingCode.js
@@ -3,16 +3,18 @@
 // let n = n
 const sum = (n) => {
   let sumOfn = 0;
-  let num = 1;
-  if (num < n) {
-    for (let i = 1; i <= n; i++) {
-      sumOfn += num;
-      num++;
-    }
-    console.log(sumOfn);
-  } else {
-    sumOfn = num;
-    console.log(sumOfn);
-  }
+  // let num = 1;
+  // if (num < n) {
+  //   for (let i = 1; i <= n; i++) {
+  //     sumOfn += num;
+  //     num++;
+  //   }
+  //   console.log(sumOfn);
+  // } else {
+  //   sumOfn = num;
+  //   console.log(sumOfn);
+  // }
+  sumOfn = (n * (n + 1)) / 2;
+  console.log(sumOfn);
 };
-sum(5);
+sum(4458796245);
